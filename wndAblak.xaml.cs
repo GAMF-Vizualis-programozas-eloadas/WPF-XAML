3c0e42d19d926d015f631d1e540053ebd3946f60
Z. Csaba Johanyák <39920181+jzscsaba@users.noreply.github.com>
2024-09-21 08:42:24 +0200

    Add files via upload

---
+++ wndAblak.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WPF_XAML
+{
+	/// <summary>
+	/// A felületet XAML-ben készítettük. A C# állomány csak a mögöttes kódot,
+	/// azaz az eseménykezelőket tartalmazza.
+	/// </summary>
+	public partial class wndAblak : Window
+  {
+    public wndAblak()
+    {
+      InitializeComponent();
+    }
+
+    private void btKilép_Click(object sender, RoutedEventArgs e)
+    {
+      // Kilépés az alkalmazásból
+      Application.Current.Shutdown();
+    }
+
+    private void btÜzenet_Click(object sender, RoutedEventArgs e)
+    {
+      // Üzenetablak megjelenítése
+      MessageBox.Show("Üzenet");
+    }
+  }
+
+}
